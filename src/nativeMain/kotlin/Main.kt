625d7645edef0705917f71f7984fa98f28b2b713
Dubuisson Samuel <sdubuisson08@gmail.com>
2023-10-29 14:21:51 +0100

    add: greedy algorithms

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -2,19 +2,22 @@ import divideAndConquer.MergeSort
 import dynamic.Fibonacci
 import dynamic.GridPath
 import dynamic.MinimumCoins
+import glutonny.GreedyMinimumCoins
 
 
 fun main(){
-    val fib = Fibonacci()
-    fib.launch();
+//    val fib = Fibonacci()
+//    fib.launch();
+//
+//    val minimumCoins = MinimumCoins();
+//    minimumCoins.launch()
+//
+//    val gridPath = GridPath();
+//    gridPath.launch();
+//
+//    val mergeSort = MergeSort();
+//    mergeSort.launch();
 
-    val minimumCoins = MinimumCoins();
-    minimumCoins.launch()
-
-    val gridPath = GridPath();
-    gridPath.launch();
-
-    val mergeSort = MergeSort();
-    mergeSort.launch();
-
+    val greedyMinimumCoins = GreedyMinimumCoins();
+    greedyMinimumCoins.launch();
 }
